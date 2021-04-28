fc1151b4a8f9cce88ab92c97daa62637aa8261a5
Profdocka <tobiasluca@ewe.net>
2021-04-28 14:48:39 +0200

    Datenbankverbindung

--- Spielzeit-Tracker/src/module-info.java
+++ Spielzeit-Tracker/src/module-info.java
@@ -1,3 +1,4 @@
 module module {
 	requires java.desktop;
+	requires java.sql;
 }
---
+++ Spielzeit-Tracker/src/sqlverbindung/Spielzeit.java
@@ -0,0 +1,13 @@
+package sqlverbindung;
+
+public class Spielzeit {
+	
+	private int spielzeitid;
+	private double spielzeit;
+	
+	public Spielzeit (int spielzeitid, double spielzeit) {
+		this.spielzeitid = spielzeitid;
+		this.spielzeit = spielzeit;
+	}
+
+}
