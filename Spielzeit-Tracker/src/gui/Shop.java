515c47fa88d2a9dc431732beff0cac613ab8b98e
agonyx <60823489+agonyx@users.noreply.github.com>
2021-05-26 13:06:29 +0200

    Changes to Haupseite- and Shop- .java | added Views.java

--- Spielzeit-Tracker/src/gui/Shop.java
+++ Spielzeit-Tracker/src/gui/Shop.java
@@ -9,6 +9,7 @@ import javax.swing.border.LineBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 import javax.swing.JTable;
 
 public class Shop extends JPanel {
@@ -21,9 +22,6 @@ public class Shop extends JPanel {
 	private JButton buttonNewButton;
 	private JLabel labelNewLabel;
 	private JTextArea textArea;
-	private JPanel panel_3;
-	private JPanel panel_4;
-	private JPanel panel_5;
 
 	/**
 	 * Create the panel.
@@ -35,7 +33,7 @@ public class Shop extends JPanel {
 	}
 	private void initComponents() {
 		setLayout(null);
-		setBounds(30, 213, 864, 723);
+		setBounds(30, 213, 865, 725);
 		{
 			panel = new JPanel();
 			panel.setBackground(Color.DARK_GRAY);
@@ -49,30 +47,30 @@ public class Shop extends JPanel {
 				panel_1.setLayout(null);
 				{
 					labelNewLabel = new JLabel("New label");
-					labelNewLabel.setBounds(10, 11, 46, 14);
+					labelNewLabel.setBounds(10, 245, 46, 14);
 					panel_1.add(labelNewLabel);
 				}
 			}
 			{
 				panel_2 = new JPanel();
-				panel_2.setBounds(10, 348, 195, 270);
+				panel_2.setBounds(21, 348, 170, 270);
 				panel.add(panel_2);
 				panel_2.setLayout(null);
 				{
 					textArea = new JTextArea();
 					textArea.setEditable(false);
-					textArea.setBounds(0, 0, 195, 270);
+					textArea.setBounds(0, 0, 174, 270);
 					panel_2.add(textArea);
 				}
 			}
 			{
 				buttonNewButton = new JButton("New button");
-				buttonNewButton.setBounds(10, 667, 89, 23);
+				buttonNewButton.setBounds(21, 667, 89, 23);
 				panel.add(buttonNewButton);
 			}
 		}
 		{
-			scrollPane = new JScrollPane();
+			scrollPane = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 			scrollPane.setBounds(228, 11, 621, 701);
 			add(scrollPane);
 			{
@@ -81,39 +79,27 @@ public class Shop extends JPanel {
 				panelViewport.setBorder(new LineBorder(new Color(0, 0, 0)));
 				scrollPane.setViewportView(panelViewport);
 				panelViewport.setLayout(null);
-				{
-					panel_3 = new JPanel();
-					panel_3.setBounds(10, 11, 256, 256);
-					panelViewport.add(panel_3);
-				}
-				{
-					panel_4 = new JPanel();
-					panel_4.setBounds(342, 11, 256, 256);
-					panelViewport.add(panel_4);
-				}
-				{
-					panel_5 = new JPanel();
-					panel_5.setBounds(10, 296, 256, 256);
-					panelViewport.add(panel_5);
-				}
 			}
 		}
 	}
 	private void createItemSections() {
 		JPanel[] j = new JPanel[itemcount];
+		JLabel[] l = new JLabel[itemcount];
 		int tabx = 0;
 		int taby = 11;
 		int count = 0;
-			for(int i = 1; i == j.length; i++) {
-				if(count<2) {
-				j[i].setBounds(10 + tabx, 11, 256, 256);
+		for(int i = 1; i == j.length; i++) {
+			if(count<2) {
+				j[i].setBounds(10 + tabx, taby, 256, 256);
+				// = Bilder einfügen lassen
+				j[i].add(l[i]);
 				tabx = tabx +332;
-				} else {
-					j[i].setBounds(10 + tabx, 11+taby, 207, 215);
-					tabx = tabx +332;
-					taby = taby +285;
-					count = 0;
-				}
+				count++;
+			} else {
+				taby = taby +285;
+				tabx = 0;
+				count = 0;				
+			}
 		}
 		panelViewport.repaint();
 	}
