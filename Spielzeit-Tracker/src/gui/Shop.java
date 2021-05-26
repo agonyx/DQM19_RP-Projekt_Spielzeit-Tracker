678b007d9983c5aa729a5ea5595e5eba981267a1
agonyx <60823489+agonyx@users.noreply.github.com>
2021-05-26 13:17:38 +0200

    fixed Shop.java

--- Spielzeit-Tracker/src/gui/Shop.java
+++ Spielzeit-Tracker/src/gui/Shop.java
@@ -1,57 +1,42 @@
-package gui;
+import java.awt.Color;
+import java.awt.Dimension;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import java.awt.Panel;
+import javax.swing.JScrollPane;
+import javax.swing.border.LineBorder;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
-import javax.swing.ScrollPaneConstants;
 import javax.swing.JTable;
-import com.jgoodies.forms.factories.DefaultComponentFactory;
-import javax.swing.border.LineBorder;
-import java.awt.Color;
-import javax.swing.SwingConstants;
 
-public class Shop extends JFrame {
-
-	private JPanel contentPane;
+public class Shop extends JPanel {
 	private JPanel panel;
+	private JScrollPane scrollPane;
+	private JPanel panelViewport;
+	private int itemcount = 10;
 	private JPanel panel_1;
 	private JPanel panel_2;
 	private JButton buttonNewButton;
 	private JLabel labelNewLabel;
 	private JTextArea textArea;
-	private JLabel lblNewLabel;
+
 	/**
-	 * Create the frame.
+	 * Create the panel.
 	 */
 	public Shop() {
+		setBackground(Color.DARK_GRAY);
 		initComponents();
+		createItemSections();
 	}
 	private void initComponents() {
-
 		setLayout(null);
 		setBounds(30, 213, 865, 725);
-		setTitle("Shop");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.DARK_GRAY);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-
 		{
 			panel = new JPanel();
-			panel.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-			panel.setBounds(10, 11, 91, 144);
-			contentPane.add(panel);
+			panel.setBackground(Color.DARK_GRAY);
+			panel.setBounds(10, 11, 215, 701);
+			add(panel);
 			panel.setLayout(null);
-
 			{
 				panel_1 = new JPanel();
 				panel_1.setBounds(21, 11, 170, 270);
@@ -59,7 +44,7 @@ public class Shop extends JFrame {
 				panel_1.setLayout(null);
 				{
 					labelNewLabel = new JLabel("New label");
-					labelNewLabel.setBounds(10, 245, 46, 14);
+					labelNewLabel.setBounds(10, 11, 46, 14);
 					panel_1.add(labelNewLabel);
 				}
 			}
@@ -71,18 +56,18 @@ public class Shop extends JFrame {
 				{
 					textArea = new JTextArea();
 					textArea.setEditable(false);
-					textArea.setBounds(0, 0, 174, 270);
+					textArea.setBounds(0, 0, 170, 270);
 					panel_2.add(textArea);
 				}
 			}
 			{
 				buttonNewButton = new JButton("New button");
-				buttonNewButton.setBounds(21, 667, 89, 23);
+				buttonNewButton.setBounds(10, 667, 89, 23);
 				panel.add(buttonNewButton);
 			}
 		}
 		{
-			scrollPane = new JScrollPane(ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			scrollPane = new JScrollPane();
 			scrollPane.setBounds(228, 11, 621, 701);
 			add(scrollPane);
 			{
@@ -96,39 +81,20 @@ public class Shop extends JFrame {
 	}
 	private void createItemSections() {
 		JPanel[] j = new JPanel[itemcount];
-		JLabel[] l = new JLabel[itemcount];
 		int tabx = 0;
 		int taby = 11;
 		int count = 0;
-		for(int i = 1; i == j.length; i++) {
-			if(count<2) {
+			for(int i = 1; i == j.length; i++) {
+				if(count<2) {
 				j[i].setBounds(10 + tabx, taby, 256, 256);
-				// = Bilder einfügen lassen
-				j[i].add(l[i]);
 				tabx = tabx +332;
 				count++;
-			} else {
-				taby = taby +285;
+				} else {
+				taby = taby + 285;
 				tabx = 0;
-				count = 0;				
-			}
+				count = 0;
+				}
 		}
 		panelViewport.repaint();
 	}
-		}
-		{
-			panel_1 = new JPanel();
-			panel_1.setBorder(new LineBorder(new Color(0, 0, 0), 2));
-			panel_1.setBounds(10, 166, 92, 84);
-			contentPane.add(panel_1);
-			panel_1.setLayout(null);
-			{
-				lblNewLabel = new JLabel("New label");
-				lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
-				lblNewLabel.setBounds(0, 0, 92, 84);
-				panel_1.add(lblNewLabel);
-			}
-		}
-	}
-
 }
