4b7e381372efb1dc48630bb2833c1312d3b8edf2
agonyx <60823489+agonyx@users.noreply.github.com>
2021-06-04 12:47:26 +0200

    fixes to DAO and DAOItems

--- Spielzeit-Tracker/src/sqlverbindung/DAOItems.java
+++ Spielzeit-Tracker/src/sqlverbindung/DAOItems.java
@@ -10,7 +10,6 @@ public class DAOItems {
 	private String database;
 	private String url;
 	private Connection conn = null;
-	ResultSet rs = null;
 
 	public DAOItems() {
 		try {
@@ -21,7 +20,8 @@ public class DAOItems {
 		database = "resources/Spielzeittracker.db";
 		url = "jdbc:sqlite:" + database;
 	}
-	//Abfrage der Items (Gesichter)
+
+	// Abfrage der Items (Gesichter)
 	public Gesichter[] getAllGesichter() throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
@@ -29,17 +29,15 @@ public class DAOItems {
 			String sql = "select * from Gesichter";
 			System.out.println(1);
 			PreparedStatement statement = conn.prepareStatement(sql);
-			System.out.println(2);
-			rs = statement.executeQuery();
-			System.out.println(3);
+			ResultSet rs = statement.executeQuery();
 			int size = getCount("Gesichter");
-			System.out.println(4);
 			Gesichter[] gesicht = new Gesichter[size];
 			int count = 0;
-			while(rs.next()) {
-				gesicht[count] = new Gesichter(rs.getInt("GesichterID"),rs.getString("Bezeichnung"), rs.getString("Bilder"));
+			do {
+				if(count <size)
+				gesicht[count] = new Gesichter(rs.getInt("GesichterID"), rs.getString("Bezeichnung"),rs.getString("Bilder"));
 				count++;
-			}
+			} while (rs.next());
 			return gesicht;
 		} catch (SQLException e) {
 			throw new DB_FehlerException(e.getMessage());
@@ -54,21 +52,23 @@ public class DAOItems {
 		}
 	}
 
-	//Abfrage der Items (Gesichtsbedeckung)
+	// Abfrage der Items (Gesichtsbedeckung)
 	public Gesichtsbedeckung[] getAllGesichtsbedeckung() throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
 
-			String sql = "select * from Gesichtsbedeckung";
+			String sql = "select * from Gesichtsbedeckungen";
 			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
-			int size = getCount("Gesichtsbedeckung");
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Gesichtsbedeckungen");
 			Gesichtsbedeckung[] gb = new Gesichtsbedeckung[size];
 			int count = 0;
-			while(rs.next()) {
-				gb[count] = new Gesichtsbedeckung(rs.getInt("GesichtsbedeckungID"),rs.getString("Bezeichnung"), rs.getString("Bilder"));
+			do {
+				if(count <size)
+				gb[count] = new Gesichtsbedeckung(rs.getInt("GBID"), rs.getString("Bezeichnung"),
+						rs.getString("Bilder"));
 				count++;
-			}
+			} while (rs.next());
 			return gb;
 		} catch (SQLException e) {
 			throw new DB_FehlerException(e.getMessage());
@@ -82,21 +82,24 @@ public class DAOItems {
 			}
 		}
 	}
-	//Abfrage der Items (Kopfbedeckung)
+
+	// Abfrage der Items (Kopfbedeckung)
 	public Kopfbedeckung[] getAllKopfbedeckung() throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
 
-			String sql = "select * from Kopfbedeckung";
+			String sql = "select * from Kopfbedeckungen";
 			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
-			int size = getCount("Kopfbedeckung");
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Kopfbedeckungen");
 			Kopfbedeckung[] Kopfbedeckung = new Kopfbedeckung[size];
 			int count = 0;
-			while(rs.next()) {
-				Kopfbedeckung[count] = new Kopfbedeckung(rs.getInt("KopfbedeckungID"),rs.getString("Bezeichnung"), rs.getString("Bilder"));
+			do {
+				if(count <size)
+				Kopfbedeckung[count] = new Kopfbedeckung(rs.getInt("KopfbedeckungenID"), rs.getString("Bezeichnung"),
+						rs.getString("Bilder"));
 				count++;
-			}
+			} while (rs.next());
 			return Kopfbedeckung;
 		} catch (SQLException e) {
 			throw new DB_FehlerException(e.getMessage());
@@ -110,21 +113,24 @@ public class DAOItems {
 			}
 		}
 	}
-	//Abfrage der Items (Koerper)
+
+	// Abfrage der Items (Koerper)
 	public Koerper[] getAllKoerper() throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
 
 			String sql = "select * from Koerper";
 			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
+			ResultSet rs = statement.executeQuery();
 			int size = getCount("Koerper");
 			Koerper[] koerper = new Koerper[size];
 			int count = 0;
-			while(rs.next()) {
-				koerper[count] = new Koerper(rs.getInt("KoerperID"),rs.getString("Bezeichnung"), rs.getString("Bilder"));
+			do {
+				if(count <size)
+				koerper[count] = new Koerper(rs.getInt("KoerperID"), rs.getString("Bezeichnung"),
+						rs.getString("Bilder"));
 				count++;
-			}
+			} while (rs.next());
 			return koerper;
 		} catch (SQLException e) {
 			throw new DB_FehlerException(e.getMessage());
@@ -138,21 +144,24 @@ public class DAOItems {
 			}
 		}
 	}
-	//Abfrage der Items (Oberteile)
+
+	// Abfrage der Items (Oberteile)
 	public Oberteil[] getAllOberteil() throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
 
 			String sql = "select * from Oberteil";
 			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
+			ResultSet rs = statement.executeQuery();
 			int size = getCount("Oberteil");
 			Oberteil[] Oberteil = new Oberteil[size];
 			int count = 0;
-			while(rs.next()) {
-				Oberteil[count] = new Oberteil(rs.getInt("OberteilID"),rs.getString("Bezeichnung"), rs.getString("Bilder"));
+			do {
+				if(count <size)
+				Oberteil[count] = new Oberteil(rs.getInt("OberteilID"), rs.getString("Bezeichnung"),
+						rs.getString("Bilder"));
 				count++;
-			}
+			} while (rs.next());
 			return Oberteil;
 		} catch (SQLException e) {
 			throw new DB_FehlerException(e.getMessage());
@@ -166,20 +175,22 @@ public class DAOItems {
 			}
 		}
 	}
-	//Abfrage der Items (Rahmen)
+
+	// Abfrage der Items (Rahmen)
 	public Rahmen[] getAllRahmen() throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
 			String sql = "select * from Rahmen";
 			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
+			ResultSet rs = statement.executeQuery();
 			int size = getCount("Rahmen");
 			Rahmen[] rahmen = new Rahmen[size];
 			int count = 0;
-			while(rs.next()) {
-				rahmen[count] = new Rahmen(rs.getInt("RahmenID"),rs.getString("Bezeichnung"), rs.getString("Bilder"));
+			do {
+				if(count <size)
+				rahmen[count] = new Rahmen(rs.getInt("RahmenID"), rs.getString("Bezeichnung"), rs.getString("Bilder"));
 				count++;
-			}
+			} while (rs.next());
 			return rahmen;
 		} catch (SQLException e) {
 			throw new DB_FehlerException(e.getMessage());
@@ -193,13 +204,14 @@ public class DAOItems {
 			}
 		}
 	}
+
 	public int getCount(String type) throws DB_FehlerException {
 		try {
 			conn = DriverManager.getConnection(url);
-			String sql = "select count(*) as count from "+type;
+			String sql = "select count(*) as count from " + type;
 			PreparedStatement statement = conn.prepareStatement(sql);
-			rs = statement.executeQuery();
-			if(rs.next()) {
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
 				return rs.getInt("count");
 			} else {
 				throw new DB_FehlerException("Fehler");
