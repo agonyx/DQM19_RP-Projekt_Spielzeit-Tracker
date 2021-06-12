e3d242656b1ac0219f36840175e3c7c61aebb288
agonyx <60823489+agonyx@users.noreply.github.com>
2021-06-12 16:53:21 +0200

    commit

--- Spielzeit-Tracker/src/sqlverbindung/DAOGetandSet.java
+++ Spielzeit-Tracker/src/sqlverbindung/DAOGetandSet.java
@@ -137,4 +137,298 @@ public class DAOGetandSet {
 		}
 		
 	}
+	// Abfrage der Items (Gesichter)
+	public Gesichter[] getAllGesichter() throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+
+			String sql = "select * from Gesichter";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Gesichter");
+			Gesichter[] gesicht = new Gesichter[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					gesicht[count] = new Gesichter(rs.getInt("GesichterID"), rs.getString("Bezeichnung"),
+							rs.getString("Bilder"));
+				count++;
+			}
+			return gesicht;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	// Abfrage der Items (Gesichtsbedeckung)
+	public Gesichtsbedeckung[] getAllGesichtsbedeckung() throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+
+			String sql = "select * from Gesichtsbedeckungen";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Gesichtsbedeckungen");
+			Gesichtsbedeckung[] gb = new Gesichtsbedeckung[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					gb[count] = new Gesichtsbedeckung(rs.getInt("GBID"), rs.getString("Bezeichnung"),
+							rs.getString("Bilder"));
+				count++;
+			} 
+			return gb;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	// Abfrage der Items (Kopfbedeckung)
+	public Kopfbedeckung[] getAllKopfbedeckung() throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+
+			String sql = "select * from Kopfbedeckungen";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Kopfbedeckungen");
+			Kopfbedeckung[] Kopfbedeckung = new Kopfbedeckung[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					Kopfbedeckung[count] = new Kopfbedeckung(rs.getInt("KopfbedeckungenID"),
+							rs.getString("Bezeichnung"), rs.getString("Bilder"));
+				count++;
+			} 
+			return Kopfbedeckung;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	// Abfrage der Items (Koerper)
+	public Koerper[] getAllKoerper() throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+
+			String sql = "select * from Koerper";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Koerper");
+			Koerper[] koerper = new Koerper[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					koerper[count] = new Koerper(rs.getInt("KoerperID"), rs.getString("Bezeichnung"),
+							rs.getString("Bilder"));
+				count++;
+			} while (rs.next());
+			return koerper;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	// Abfrage der Items (Oberteile)
+	public Oberteil[] getAllOberteil() throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+
+			String sql = "select * from Oberteil";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Oberteil");
+			Oberteil[] Oberteil = new Oberteil[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					Oberteil[count] = new Oberteil(rs.getInt("OberteilID"), rs.getString("Bezeichnung"),
+							rs.getString("Bilder"));
+				count++;
+			} while (rs.next());
+			return Oberteil;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	// Abfrage der Items (Rahmen)
+	public Rahmen[] getAllRahmen() throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+			String sql = "select * from Rahmen";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Rahmen");
+			Rahmen[] rahmen = new Rahmen[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					rahmen[count] = new Rahmen(rs.getInt("RahmenID"), rs.getString("Bezeichnung"),
+							rs.getString("Bilder"));
+				count++;
+			} while (rs.next());
+			return rahmen;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	// Erstellen eines angezeigten Benutzeravatars
+	public void insertAvatarItems(Benutzer benutzer, Rahmen rahmen, Gesichter gesichter,
+			Gesichtsbedeckung gesichtsbedeckung, Kopfbedeckung kopfbedeckung, Oberteil oberteil, Koerper koerper)
+			throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+			String sql = "Insert Into Avatar (BenutzerID, RahmenID, GesichterID, GBID, KopfbedeckungenID, OberteilID, AvatarbildID) Values (?,?,?,?,?,?,?)";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement.setInt(1, benutzer.getID());
+			statement.setInt(2, rahmen.getRahmenID());
+			statement.setInt(3, gesichter.getGesichterID());
+			statement.setInt(4, gesichtsbedeckung.getGBID());
+			statement.setInt(5, kopfbedeckung.getKopfbedeckungsID());
+			statement.setInt(6, oberteil.getOberteilID());
+			statement.setInt(7, koerper.getKoerperID());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
+
+	public void changeAvatar(int benutzerid, Rahmen rahmen, Gesichter gesichter, Gesichtsbedeckung gesichtsbedeckung, Kopfbedeckung kopfbedeckung, Oberteil oberteil, Koerper koerper) throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+			String sql = "update Avatar Set RahmenID = ?, GesichterID = ?, GBID = ?, KopfbedeckungenID = ?, OberteilID = ?, AvatarbildID = ? where BenutzerID = ?";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			statement = conn.prepareStatement(sql);
+			statement.setInt(1, rahmen.getRahmenID());
+			statement.setInt(2, gesichter.getGesichterID());
+			statement.setInt(3, gesichtsbedeckung.getGBID());
+			statement.setInt(4, kopfbedeckung.getKopfbedeckungsID());
+			statement.setInt(5, oberteil.getOberteilID());
+			statement.setInt(6, koerper.getKoerperID());
+			statement.setInt(7, benutzerid);
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+
+		}
+	}
+
+	public int getCount(String type) throws DB_FehlerException {
+		try {
+			conn = DriverManager.getConnection(url);
+			String sql = "select count(*) as count from " + type;
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			if (rs.next()) {
+				return rs.getInt("count");
+			} else {
+				throw new DB_FehlerException("Fehler");
+			}
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}	
+	public void getAllGames() {
+		try {
+			conn = DriverManager.getConnection(url);
+
+			String sql = "select * from Spiele";
+			PreparedStatement statement = conn.prepareStatement(sql);
+			ResultSet rs = statement.executeQuery();
+			int size = getCount("Gesichter");
+			Gesichter[] gesicht = new Gesichter[size];
+			int count = 0;
+			while (rs.next()) {
+				if (count < size)
+					gesicht[count] = new Gesichter(rs.getInt("GesichterID"), rs.getString("Bezeichnung"),
+							rs.getString("Bilder"));
+				count++;
+			}
+			return gesicht;
+		} catch (SQLException e) {
+			throw new DB_FehlerException(e.getMessage());
+		} finally {
+			try {
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				throw new DB_FehlerException(e.getMessage());
+			}
+		}
+	}
 }
